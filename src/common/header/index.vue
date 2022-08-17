9bc071603171ca6b1efd641bbf608f2d6249c7ea
caipaofan <954698621@qq.com>
2022-08-17 18:03:48 +0800

    完成学科模块

---
+++ src/common/header/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-row type="flex">
+      <el-col :span="20">
+        <el-row type="flex">
+          <el-col :span="8"><slot name="input1"></slot></el-col>
+          <el-col :span="16">
+            <el-row type="flex" justify="start">
+              <slot name="input2" ></slot>
+              <el-button size="small" >清除</el-button>
+              <el-button type="primary" size="small" >搜索</el-button>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="8">
+        <el-row type="flex" justify="end">
+          <newButton :text="text"></newButton>
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-form>
+</template>
+
+<script>
+import newButton from "@/common/newButton";
+export default {
+  data() {
+    return {
+        form
+    };
+  },
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    newButton,
+  },
+  created() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped></style>
--- src/module-hmmm/pages/subjects.vue
+++ src/module-hmmm/pages/subjects.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class='container'>学科管理</div>
+  <div class="container">
+    <pannel>
+      <headerya text="新增学科">
+        <template #input1>
+          <el-form-item label="学科名称">
+            <el-input></el-input>
+          </el-form-item>
+        </template>
+      </headerya>
+    </pannel>
+  </div>
 </template>
 
 <script>
-export default {}
+import pannel from "@/common/pannel";
+import headerya from "@/common/header";
+export default {
+  components: {
+    pannel,
+    headerya,
+  },
+};
 </script>
 
-<style scoped lang='less'></style>
+<style scoped >
+.el-input{
+  width: 190px;
+}
+</style>
