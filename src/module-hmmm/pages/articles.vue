13cd685691836830308f0b1d90228da4c18f2e67
shijinbao1996 <1366221851@qq.com>
2022-08-17 13:08:13 +0800

    完成简单功能

--- src/module-hmmm/pages/articles.vue
+++ src/module-hmmm/pages/articles.vue
@@ -2,23 +2,35 @@
   <div class="dashboard-container">
     <div class="app-container">
       <el-card>
-        <el-form ref="form" label-width="80px">
+        <el-form ref="form" :model="headForm" label-width="80px">
           <el-row>
             <el-col :span="6">
-              <el-form-item label="关键字">
-                <el-input placeholder="根据文章标题搜索"></el-input>
+              <el-form-item label="关键字" prop="keyword">
+                <el-input
+                  placeholder="根据文章标题搜索"
+                  v-model="headForm.keyword"
+                ></el-input>
               </el-form-item>
             </el-col>
             <el-col :span="6">
-              <el-form-item label="状态">
-                <el-select placeholder="请选择">
-                  <el-option label="区域一" value="shanghai"></el-option>
+              <el-form-item label="状态" prop="state">
+                <el-select
+                  placeholder="请选择"
+                  v-model="headForm.state"
+                  @focus="selFocus"
+                >
+                  <el-option
+                    :label="item.label"
+                    :value="item.value"
+                    v-for="item in status"
+                    :key="item.value"
+                  ></el-option>
                 </el-select>
               </el-form-item>
             </el-col>
             <el-col :span="6" style="padding-left: 40px">
-              <el-button>清除</el-button>
-              <el-button type="primary">搜索</el-button>
+              <el-button @click="clearBtn">清除</el-button>
+              <el-button type="primary" @click="findBtn">搜索</el-button>
             </el-col>
             <el-col :span="6" style="text-align: right">
               <el-button type="success" icon="el-icon-edit">新增技巧</el-button>
@@ -38,7 +50,6 @@
           :data="tableData"
           style="width: 100%"
           :header-cell-style="{ background: '#fafafa' }"
-          :cell-style="rowStyle"
         >
           <el-table-column
             prop="date"
@@ -49,35 +60,35 @@
           >
           </el-table-column>
           <el-table-column
-            prop="name"
+            prop="title"
             label="文章标题"
             width="400"
             align="center"
           >
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="visits"
             label="阅读数"
             width="295"
             align="center"
           >
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="username"
             label="录入人"
             width="294"
             align="center"
           >
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="createTime"
             label="录入时间"
             width="294"
             align="center"
           >
           </el-table-column>
           <el-table-column
-            prop="address"
+            prop="cnState"
             label="状态"
             width="100"
             align="center"
@@ -90,17 +101,29 @@
             align="center"
           >
             <template slot-scope="scope">
-              <el-button size="medium" type="text">预览</el-button>
-              <el-button size="medium" type="text">禁用</el-button>
+              <el-button size="medium" type="text" @click="previewBtn(scope)"
+                >预览</el-button
+              >
+              <el-button
+                size="medium"
+                v-if="(scope.row.state = 1)"
+                type="text"
+                @click="changeState(scope)"
+                >启用</el-button
+              >
+              <el-button
+                size="medium"
+                v-else
+                type="text"
+                @click="changeState(scope)"
+                >禁用</el-button
+              >
               <el-button size="medium" type="text" disabled>修改</el-button>
               <el-button size="medium" type="text" disabled>删除</el-button>
             </template>
           </el-table-column>
         </el-table>
         <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="currentPage4"
           :page-sizes="[10, 20, 30, 50]"
           :page-size="100"
           :total="300"
@@ -110,38 +133,121 @@
         >
         </el-pagination>
       </el-card>
+      <el-dialog title="预览文章" :visible.sync="previewDialog" width="50%">
+        <div>
+          <div class="title">
+            <h2>{{ title }}</h2>
+            <div>
+              <span>{{ createTime }}</span>
+              <span>{{ username }}</span>
+              <span class="el-icon-view"></span>
+              <span>{{ visits }}</span>
+            </div>
+          </div>
+          <div class="content" v-html="articleBody"></div>
+        </div>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
+import { profile } from "@/api/base/frame";
+import { listApi, changeStateApi } from "@/api/hmmm/articles";
+import { status } from "@/api/hmmm/constants";
 export default {
   data() {
     return {
       recordNum: 10,
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
+      page: 1,
+      pagesize: 10,
+      tableData: [],
+      headForm: {
+        keyword: null,
+        state: null,
+      },
+      status: [],
+
+      previewDialog: false,
+      title: "",
+      visits: "",
+      username: "",
+      createTime: "",
+      articleBody: "",
     };
+  },
+  created() {
+    this.list();
+  },
+  methods: {
+    async list() {
+      const res = await listApi();
+      console.log(res);
+      res.data.items.forEach((item) => {
+        if (item.state === 1) {
+          return (item.cnState = "已启用");
+        } else {
+          return (item.cnState = "已禁用");
+        }
+      });
+      console.log(res.data.items);
+      this.tableData = res.data.items;
+      this.recordNum = res.data.counts;
+    },
+    async clearBtn() {
+      this.$refs.form.resetFields();
+      const res = await listApi();
+      this.tableData = res.data.items;
+      this.recordNum = res.data.counts;
+    },
+    selFocus() {
+      this.status = status;
+    },
+    async findBtn() {
+      const data = {
+        keyword: this.headForm.keyword,
+        state: this.headForm.state,
+        page: this.page,
+        pagesize: this.pagesize,
+      };
+      const res = await listApi(data);
+      console.log(res);
+      this.tableData = res.data.items;
+      this.recordNum = res.data.counts;
+    },
+    previewBtn(scope) {
+      this.previewDialog = true;
+      this.title = scope.row.title;
+      this.createTime = scope.row.createTime;
+      this.username = scope.row.username;
+      this.visits = scope.row.visits;
+      this.articleBody = scope.row.articleBody;
+    },
+    async changeState(scope) {
+      const data = {
+        id: scope.row.id,
+        state: scope.row.state,
+      };
+      const {
+        data: { success },
+      } = await changeStateApi(data);
+      console.log(success);
+      if (success) {
+        if (scope.row.state === 1) {
+          scope.row.state = 0;
+          scope.row.cnState = "已禁用";
+        } else {
+          scope.row.state = 1;
+          scope.row.cnState = "已启用";
+        }
+        this.$message({
+          message: "操作成功",
+          type: "success",
+        });
+      } else {
+        this.$message.error("操作失败");
+      }
+    },
   },
 };
 </script>
@@ -165,5 +271,17 @@ export default {
   color: #f56c6c;
   background: #fef0f0;
   border-color: #fbc4c4;
+}
+.title h2 {
+  padding: 0;
+  margin: 0;
+}
+.title span {
+  margin-left: 10px;
+  margin-top: 10px;
+}
+.content {
+  background: #f5f5f5;
+  padding: 10px;
 }
 </style>
